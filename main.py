c4994cbc5a0c5620e9e0e73db48a9f04371d99ea
Drew White <drew.m.white51@gmail.com>
2022-07-23 13:44:33 -0500

    Updates to stock object.

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import yfinance
 from web_scraper import StockScraper
 import pprint
 import postgres_connection
-
+import stock
 watch_list = ['nvda', 'ibm', 'f', 'v', 'voo', 'spy']
 
 # postgres_connection.compare_watchlist(watch_list)
 
+print(stock.Stock('ibm').get_hourly_data())
+
+#
+# for data in get_data('ibm'):
+#     print(data)
--- stock.py
+++ stock.py
@@ -12,21 +12,21 @@ class Stock:
         self.ticker = ticker
 
     def return_stock_information(self):
-        return yfinance.Ticker(self.ticker.info['shortName'])
+        return yfinance.Ticker((self.ticker).info['shortName'])
 
     def get_daily_data(self):
         # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
         # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-        return yfinance.Ticker(self.ticker.history(period='10y', interval='1d'))
+        return yfinance.Ticker(self.ticker).history(period='10y', interval='1d')
 
     def get_hourly_data(self):
-        return yfinance.Ticker(self.ticker.history(period='1y', interval='1h'))
+        return yfinance.Ticker(self.ticker).history(period='1y', interval='1h')
 
     def get_30m_data(self):
-        return yfinance.Ticker(self.ticker.history(period='1mo', interval='30m'))
+        return yfinance.Ticker(self.ticker).history(period='1mo', interval='30m')
 
     def get_min_data(self):
-        return yfinance.Ticker(self.ticker.history(period='5d', interval='1m'))
+        return yfinance.Ticker(self.ticker).history(period='5d', interval='1m')
 
     def get_fundamental_data(self):
         stock = yfinance.Ticker(self.ticker)
